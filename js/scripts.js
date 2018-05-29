16b504626d1926c94b779485cfa46a4369d34099
Annie Shin and Reese Glasscock <annie@>
2018-05-29 11:04:31 -0700

    add javascript for BMI calculator

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,34 @@
-var add = function(number1, number2) {
-  return number1 + number2;
+// var add = function(number1, number2) {
+//   return number1 + number2;
+// };
+//
+// var subtract = function(number1, number2) {
+//   return number1 - number2;
+// };
+//
+// var multiply = function(number1, number2) {
+//   return number1 * number2;
+// };
+//
+// var divide = function(number1, number2) {
+//   return number1 / number2;
+// };
+//
+// var number1 = parseInt(prompt("Enter a number:"));
+// var number2 = parseInt(prompt("Enter another number:"));
+//
+// var result = divide(number1, number2);
+// alert(result);
+
+
+//BMI Calculator
+var divide = function(weight, height) {
+  return weight / height;
 };
 
-var number1 = parseInt(prompt("Enter a number:"));
-var number2 = parseInt(prompt("Enter another number:"));
+var weight = parseInt(prompt("Enter your weight (in pounds):"));
+var height = parseInt(prompt("Enter your height (in inches):"));
+var height = height * height;
 
-alert(add(number1, number2));
+var result = divide(weight, height)*703.0704;
+alert(result);

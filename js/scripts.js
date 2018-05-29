ae09f33e863263e5b2c09b56c35e58e761e5ad47
Annie Shin and Reese Glasscock <annie+reese@>
2018-05-29 11:44:57 -0700

    add the calculation for cups to gallons

--- js/scripts.js
+++ js/scripts.js
@@ -22,13 +22,39 @@
 
 
 //BMI Calculator
-var divide = function(weight, height) {
-  return weight / height;
+// var divide = function(weight, height) {
+//   return weight / height;
+// };
+//
+// var weight = parseInt(prompt("Enter your weight (in pounds):"));
+// var height = parseInt(prompt("Enter your height (in inches):"));
+// var height = height * height;
+//
+// var result = divide(weight, height)*703.0704;
+// alert(result);
+
+//Temperature Conversion - Celsius and Fahrenheit
+
+//
+// var tempF = function(celsius) {
+//     return celsius;
+// }
+//
+// var celsius = parseInt(prompt("What is the temperature in celsius?"));
+// var result = "The temperature in Fahrenheit would be " + (((9/5)*celsius) + 32);
+//
+// alert(result);
+//
+//
+
+
+// Cups to Gallons
+
+var gallons = function(cups) {
+  return cups;
 };
 
-var weight = parseInt(prompt("Enter your weight (in pounds):"));
-var height = parseInt(prompt("Enter your height (in inches):"));
-var height = height * height;
+var cups = parseInt(prompt("How many cups do you have?"));
 
-var result = divide(weight, height)*703.0704;
+var result = ("You have a total of " + cups/16 + " gallons.");
 alert(result);

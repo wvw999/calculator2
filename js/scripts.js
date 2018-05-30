c826291624102eefdb1093f15a7cfc288b30bb0b
Eliran Maimon and Reese Glasscock <reese@>
2018-05-30 15:11:24 -0700

    add click function for calculator to have function for add, subtract, divide, and multiply

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,57 @@
-// var add = function(number1, number2) {
-//   return number1 + number2;
-// };
-//
-// var subtract = function(number1, number2) {
-//   return number1 - number2;
-// };
-//
-// var multiply = function(number1, number2) {
-//   return number1 * number2;
-// };
-//
-// var divide = function(number1, number2) {
-//   return number1 / number2;
-// };
-//
-// var number1 = parseInt(prompt("Enter a number:"));
-// var number2 = parseInt(prompt("Enter another number:"));
-//
-// var result = divide(number1, number2);
-// alert(result);
+// Business (or back-end) logic:
+
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  return number1 / number2;
+};
+
+// User interface (or front-end) logic:
+
+$(document).ready(function(){
+  $("button#add").click(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = add(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("button#subtract").click(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = subtract(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("button#multiply").click(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = multiply(number1, number2);
+    $("#output").text(result);
+  });
+
+  $("button#divide").click(function(event){
+    event.preventDefault();
+    var number1 = parseInt($("#number1").val());
+    var number2 = parseInt($("#number2").val());
+    var result = divide(number1, number2);
+    $("#output").text(result);
+  });
+});
+
 
 
 //BMI Calculator
@@ -50,11 +83,11 @@
 
 // Cups to Gallons
 
-var gallons = function(cups) {
-  return cups;
-};
-
-var cups = parseInt(prompt("How many cups do you have?"));
-
-var result = ("You have a total of " + cups/16 + " gallons.");
-alert(result);
+// var gallons = function(cups) {
+//   return cups;
+// };
+//
+// var cups = parseInt(prompt("How many cups do you have?"));
+//
+// var result = ("You have a total of " + cups/16 + " gallons.");
+// alert(result);

f6d5aaa3249b9f539f8acf0f0d5c3e45d9b34c97
Reese Glasscock and Will Swanson <reese@>
2018-05-31 15:16:33 -0700

    build calc functions and js to make them work

--- js/scripts.js
+++ js/scripts.js
@@ -19,35 +19,23 @@ var divide = function(number1, number2) {
 // User interface (or front-end) logic:
 
 $(document).ready(function(){
-  $("button#add").click(function(event){
+  $("form#calc").submit(function(event){
     event.preventDefault();
     var number1 = parseInt($("#number1").val());
     var number2 = parseInt($("#number2").val());
-    var result = add(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("button#subtract").click(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#number1").val());
-    var number2 = parseInt($("#number2").val());
-    var result = subtract(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("button#multiply").click(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#number1").val());
-    var number2 = parseInt($("#number2").val());
-    var result = multiply(number1, number2);
-    $("#output").text(result);
-  });
-
-  $("button#divide").click(function(event){
-    event.preventDefault();
-    var number1 = parseInt($("#number1").val());
-    var number2 = parseInt($("#number2").val());
-    var result = divide(number1, number2);
+    var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    } else {
+      alert("You need to select something there, smart guy");
+    }
     $("#output").text(result);
   });
 });
